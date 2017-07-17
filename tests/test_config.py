5f68c19ac57b2f02cde0715a5be32fce4aad5186
Gustavo Carneiro <gustavocarneiro@gambitresearch.com>
2017-07-17 19:21:48 +0100

    tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,3 +3,17 @@ from yacron import config
 
 def test_mergedicts():
 	assert dict(config.mergedicts({"a": 1}, {"b": 2})) == {"a": 1, "b": 2}
+
+
+def test_mergedicts_nested():
+	assert dict(config.mergedicts(
+	    {"a": {'x': 1, 'y': 2, 'z': 3}},
+	    {'a': {'y': 10}, "b": 2})) == \
+		{"a": {'x': 1, 'y': 10, 'z': 3}, "b": 2}
+
+
+def test_mergedicts_right_none():
+	assert dict(config.mergedicts(
+	    {"a": {'x': 1}},
+	    {"a": None, "b": 2})) == \
+		{"a": {'x': 1}, "b": 2}
