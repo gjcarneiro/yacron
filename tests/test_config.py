e5351bba6cdf7b76da895afda80c18309c7f90eb
Delirious Lettuce <delirious.lettuce@gmail.com>
2017-07-19 16:23:23 -0600

    Replace tabs with spaces, use parens to get rid of backslashes

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,26 +2,25 @@ from yacron import config
 
 
 def test_mergedicts():
-	assert dict(config.mergedicts({"a": 1}, {"b": 2})) == {"a": 1, "b": 2}
+    assert dict(config.mergedicts({"a": 1}, {"b": 2})) == {"a": 1, "b": 2}
 
 
 def test_mergedicts_nested():
-	assert dict(config.mergedicts(
-	    {"a": {'x': 1, 'y': 2, 'z': 3}},
-	    {'a': {'y': 10}, "b": 2})) == \
-		{"a": {'x': 1, 'y': 10, 'z': 3}, "b": 2}
+    assert (dict(config.mergedicts(
+        {"a": {'x': 1, 'y': 2, 'z': 3}},
+        {'a': {'y': 10}, "b": 2}
+    )) == {"a": {'x': 1, 'y': 10, 'z': 3}, "b": 2})
 
 
 def test_mergedicts_right_none():
-	assert dict(config.mergedicts(
-	    {"a": {'x': 1}},
-	    {"a": None, "b": 2})) == \
-		{"a": {'x': 1}, "b": 2}
+    assert (dict(config.mergedicts(
+        {"a": {'x': 1}},
+        {"a": None, "b": 2}
+    )) == {"a": {'x': 1}, "b": 2})
 
 
 def test_mergedicts_lists():
-	assert dict(config.mergedicts(
-	    {"env": [{'key': 'FOO'}]},
-	    {"env": [{'key': 'BAR'}]})) \
-		== \
-		{"env": [{'key': 'FOO'}, {'key': 'BAR'}]}
+    assert (dict(config.mergedicts(
+        {"env": [{'key': 'FOO'}]},
+        {"env": [{'key': 'BAR'}]}
+    )) == {"env": [{'key': 'FOO'}, {'key': 'BAR'}]})
