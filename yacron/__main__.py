391fda25884f97766f48714f8d4c438ccaf1596b
Gustavo Carneiro <gjcarneiro@gmail.com>
2022-12-31 17:06:05 +0000

    give a better error message when no configuration file is provided or exists, fixes #72

--- yacron/__main__.py
+++ yacron/__main__.py
@@ -4,18 +4,27 @@ import asyncio.subprocess
 import logging
 import signal
 import sys
+import os
 
 from yacron.cron import Cron, ConfigError
 import yacron.version
 
+CONFIG_DEFAULT = "/etc/yacron.d"
+
 
 def main_loop(loop):
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog="yacron")
     parser.add_argument(
-        "-c", "--config", default="/etc/yacron.d", metavar="FILE-OR-DIR"
+        "-c",
+        "--config",
+        default=CONFIG_DEFAULT,
+        metavar="FILE-OR-DIR",
+        help="configuration file, or directory containing configuration files",
     )
     parser.add_argument("-l", "--log-level", default="INFO")
-    parser.add_argument("-v", "--validate-config", default=False, action="store_true")
+    parser.add_argument(
+        "-v", "--validate-config", default=False, action="store_true"
+    )
     parser.add_argument("--version", default=False, action="store_true")
     args = parser.parse_args()
 
@@ -26,6 +35,15 @@ def main_loop(loop):
     if args.version:
         print(yacron.version.version)
         sys.exit(0)
+
+    if args.config == CONFIG_DEFAULT and not os.path.exists(args.config):
+        print(
+            "yacron error: configuration file not found, please provide one "
+            "with the --config option",
+            file=sys.stderr,
+        )
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
     try:
         cron = Cron(args.config)
